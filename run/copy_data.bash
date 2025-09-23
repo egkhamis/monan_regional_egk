642e1d035ee4ac2cc62b31db8b7cf2ab6c93324b
egkhamis <egkhamis@gmail.com>
2025-09-22 23:52:06 -0300

    1st attempt to copy specific gfs data

--- run/copy_data.bash
+++ run/copy_data.bash
@@ -2,6 +2,15 @@
 path_prefix_in="/mnt/beegfs/monan/714/monan_oper/monan"
 path_prefix_out=`cd ..;pwd`
 
+# Input variables:-----------------------------------------------------
+#EXP=${1};         #EXP=GFS
+#RES=${1};         #RES=1024002
+YYYYMMDDHHi=${1}; #YYYYMMDDHHi=2024012000
+#echo "EXP: ${EXP}"
+#echo "RES: ${RES}"
+echo "LABELI: ${YYYYMMDDHHi}"
+#----------------------------------------------------------------------
+
 # Define a function with local variables
 
 copy_mesh_quasi_uniform() {
@@ -57,7 +66,7 @@ copy_mesh_WPS_GEOG() {
 # Define a function with local variables
 
 copy_mesh_datain_gfs() {
-  local path_in=${1}/pre/datain/regional/gfs/
+  local path_in§=${1}/pre/datain/regional/gfs/
   local path_out=${2}/pre/datain/regional/gfs/
 
   echo "cp -urfp: ${path_in} ${path_in}"
@@ -111,10 +120,49 @@ copy_mesh_tables() {
   
   cp -urfp ${path_in}/* ${path_out}/
 }
+
+# Define a function with local variables
+
+copy_datain_gfs() {
+
+  local path_in=${1}/pre/datain/regional/gfs/
+  local path_out=${2}/pre/datain/regional/gfs/
+  local data=${3}
+
+  mkdir -p ${path_in}/${data}
+
+#  echo "cp -urfp: ${path_in} ${path_in}"
+#  
+#  cp -urfp ${path_in}/* ${path_out}/
+#
+#  local path_in=${1}/pre/datain/global/gfs/
+#  local path_out=${2}/pre/datain/global/gfs/
+#
+#  echo "cp -urfp: ${path_in} ${path_in}"
+#  
+#  cp -urfp ${path_in}/* ${path_out}/
+#
+#
+#  mkdir -p pre/datain/regional/gfs/2025071500
+#
+#  cd pre/datain/regional/gfs/2025071500
+#
+#  for i in $(seq -w 000 3 048); do cp /oper/dados/ioper/tempo/GFS/0p25/brutos/2025/07/15/00/gfs.t00z.pgrb2.0p25.f${i}.* . ; done
+#
+#  cp /mnt/beegfs/monan/CIs/GFS/2025/2025071500/GFS:2025-07-15_00 .
+
+}
+
+# (if) se nao ha flag nenhuma
 copy_mesh_quasi_uniform       ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_variable_resolution ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_WPS_GEOG            ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_datain_gfs          ${path_prefix_in}  ${path_prefix_out}
-copy_mesh_datain_era5         ${path_prefix_in}  ${path_prefix_out}
+#copy_mesh_datain_era5         ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_exec                ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_tables              ${path_prefix_in}  ${path_prefix_out}
+
+# (if) se tem flag da data
+#copy_datain_gfs               ${path_prefix_in}  ${path_prefix_out} ${YYYYMMDDHHi}
+
+
